424df54096b601bbd633e1ad802f29768301be26
Vin Bui <vdb23@cornell.edu>
2024-03-18 22:50:55 -0400

    Finish cell

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # lec4-uitableview
 Demo code for Lecture 4: UITableView
-
-There are 2 branches:
-- `1-cell`: implemented UITableViewCell
-- `2-tableview`: implemented UITableView
---
+++ lec4/Views/BirdTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  BirdTableViewCell.swift
+//  lec4
+//
+//  Created by Vin Bui on 6/7/23.
+//
+
+import UIKit
+
+class BirdTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties (view)
+    
+    private let birdImage = UIImageView()
+    private let birdNameLabel = UILabel()
+    
+    // MARK: - init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupBirdImage()
+        setupNameLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set Up Views
+    
+    private func setupBirdImage() {
+        birdImage.contentMode = .scaleAspectFit
+        
+        // NOTE: we are using `contentView` here
+        contentView.addSubview(birdImage)
+        birdImage.translatesAutoresizingMaskIntoConstraints = false
+                        
+        // NOTE: we do not need to use `safeAreaLayoutGuide`
+        NSLayoutConstraint.activate([
+            birdImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            birdImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            birdImage.widthAnchor.constraint(equalToConstant: 64)
+        ])
+    }
+    
+    private func setupNameLabel() {
+        birdNameLabel.textColor = .label
+        birdNameLabel.font = .systemFont(ofSize: 20, weight: .medium)
+        
+        contentView.addSubview(birdNameLabel)
+        birdNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            birdNameLabel.leadingAnchor.constraint(equalTo: birdImage.trailingAnchor, constant: 8),
+            birdNameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            birdNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+        ])
+    }
+    
+}
